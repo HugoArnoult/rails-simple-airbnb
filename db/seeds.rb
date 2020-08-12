74dee31eb687b696fce6bf9d608b3854bedca572
Hugo Arnoult <hugo.arnoult83@gmail.com>
2020-08-12 17:11:29 +0200

    Add 4 flats with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 4 fake flats...'
+4.times do
+  flat = Flat.new(
+    name: Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+    price_per_night: Faker::Number.between(from: 50, to: 500),
+    number_of_guests: Faker::Number.between(from: 1, to: 12),
+  )
+  flat.save!
+end
+puts 'Finished!'
